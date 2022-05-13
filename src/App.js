7e1490c1216fba2adda9f2da497df359566b7ae3
tonyxuan2021 <tonyyexuan@gmail.com>
2022-05-12 20:10:10 -0700

    complete initial pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import Navbar from "./component/Navbar/Navbar";
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import InspirationsPage from "./pages/InspirationsPage/InspirationsPage";
 
 function App() {
   return (
     <div className="App">
-    <Navbar />
-
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/inspirations" component={InspirationsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,49 +1,52 @@
-import React from 'react'
+import React from "react";
 import logo from "../../assets/shutterstock-logo-pos 1.png";
 import downArrow from "../../assets/ic_round-arrow-back-ios-new.png";
-import heart from "../../assets/bi_suit-heart.png"
+import heart from "../../assets/bi_suit-heart.png";
 import avatar_2 from "../../assets/Ellipse 2.png";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='nav__wrapper'>
-    <img src={logo} className="logo"></img>
-    <div className='link__wrapper'>
-      <p>INSPIRATIONS</p>
-      <img src={downArrow} className="downArrow"></img>
+    <div className="nav__wrapper">
+      <Link to={"/"}>
+        <img src={logo} className="logo"></img>
+      </Link>
+      <Link to={"/inspirations"} className="link__wrapper">
+        <p>INSPIRATIONS</p>
+        <img src={downArrow} className="downArrow"></img>
+      </Link>
+      <div className="link__wrapper">
+        <p>IMAGES</p>
+        <img src={downArrow} className="downArrow"></img>
+      </div>
+      <div className="link__wrapper">
+        <p>VIDEO</p>
+        <img src={downArrow} className="downArrow"></img>
+      </div>
+      <div className="link__wrapper">
+        <p>MISIC</p>
+        <img src={downArrow} className="downArrow"></img>
+      </div>
+      <div className="link__wrapper">
+        <p>TEMPLATES</p>
+        <img src={downArrow} className="downArrow"></img>
+      </div>
+      <div className="link__wrapper">
+        <p>EDITORIAL</p>
+        <img src={downArrow} className="downArrow border"></img>
+      </div>
+      <p>ENTERPRISE</p>
+      <p className="radius">Get $50</p>
+      <p className="radius background">Submit a photo</p>
+      <div className="link__wrapper">
+        <p>Pricing</p>
+        <img src={downArrow} className="downArrow"></img>
+      </div>
+      <img src={heart} className="nav__img"></img>
+      <img src={avatar_2} className="nav__img"></img>
     </div>
-    <div className='link__wrapper'>
-      <p>IMAGES</p>
-      <img src={downArrow} className="downArrow"></img>
-    </div>
-    <div className='link__wrapper'>
-      <p>VIDEO</p>
-      <img src={downArrow} className="downArrow"></img>
-    </div>
-    <div className='link__wrapper'>
-      <p>MISIC</p>
-      <img src={downArrow} className="downArrow"></img>
-    </div>
-    <div className='link__wrapper'>
-      <p>TEMPLATES</p>
-      <img src={downArrow} className="downArrow"></img>
-    </div>
-    <div className='link__wrapper'>
-      <p>EDITORIAL</p>
-      <img src={downArrow} className="downArrow border"></img>
-    </div>
-    <p>ENTERPRISE</p>
-    <p className='radius'>Get $50</p>
-    <p className='radius background'>Submit a photo</p>
-    <div className='link__wrapper'>
-      <p>Pricing</p>
-      <img src={downArrow} className="downArrow"></img>
-    </div>
-    <img src={heart} className="nav__img"></img>
-    <img src={avatar_2} className="nav__img"></img>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
